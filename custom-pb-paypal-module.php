310cbc322f4a9c64394cd7e16053929d303b2e77
tejas <tejasmht88@gmail.com>
2017-01-30 18:02:46 +0530

    Added selection box for divi module to select either to use donate or buynow. ref #1

--- custom-pb-paypal-module.php
+++ custom-pb-paypal-module.php
@@ -10,13 +10,13 @@ function ex_divi_child_theme_setup1() {
 		$this->slug = 'et_pb_paypal_button';
 
 		$this->whitelisted_fields = array(
-			'button_text',
+                        'pp_select_button',
+			//'button_text',
 			'pp_item_name',
 			'pp_amount',
 			'admin_label',
 			'module_id',
-			'module_class',
-                        'text_orientation'
+			'module_class'
 		);
                 
 		$this->main_css_element = '%%order_class%%';
@@ -35,12 +35,22 @@ function ex_divi_child_theme_setup1() {
 
 	function get_fields() {
 		$fields = array(
-			'button_text' => array(
-				'label'           => esc_html__( 'Button Text', 'et_builder' ),
-				'type'            => 'text',
+                        'pp_select_button' => array(
+				'label'           => esc_html__( 'Select Button', 'et_builder' ),
+				'type'            => 'select',
 				'option_category' => 'basic_option',
-				'description'     => esc_html__( 'Input your desired button text.', 'et_builder' ),
+				'options'         => array(
+					'buynow'  => esc_html__( 'Buy now', 'et_builder' ),
+					'donate'  => esc_html__( 'Donate', 'et_builder' ),
+				),
+				'description' => esc_html__( 'Here you can choose whether to use PayPal *Buy Now* Button or *Donate* Button ', 'et_builder' ),
 			),
+//			'button_text' => array(
+//				'label'           => esc_html__( 'Button Text', 'et_builder' ),
+//				'type'            => 'text',
+//				'option_category' => 'basic_option',
+//				'description'     => esc_html__( 'Input your desired button text.', 'et_builder' ),
+//			),
                         'pp_item_name' => array(
                          	'label'           => esc_html__( 'Paypal Item name', 'et_builder' ),
 				'type'            => 'text',
@@ -79,34 +89,46 @@ function ex_divi_child_theme_setup1() {
 	function shortcode_callback( $atts, $content = null, $function_name ) {
 		$module_id         = $this->shortcode_atts['module_id'];
 		$module_class      = $this->shortcode_atts['module_class'];		
-		$button_text       = $this->shortcode_atts['button_text'];	
+		//$button_text       = $this->shortcode_atts['button_text'];	
                 $pp_item_name      = $this->shortcode_atts['pp_item_name'];
                 $pp_amount         = $this->shortcode_atts['pp_amount'];
 		$custom_icon       = $this->shortcode_atts['button_icon'];
 		$button_custom     = $this->shortcode_atts['custom_button'];
 		$button_alignment  = $this->shortcode_atts['button_alignment'];
+                $pp_select_button  = $this->shortcode_atts['pp_select_button'];
 
 		// Nothing to output if neither Button Text defined
-		if ( '' === $button_text) {
-			return;
-		}
-
+		//if ( '' === $button_text) {
+		//	return;
+		//}
+                if($pp_select_button =='buynow'){
+                    $cmd    = '_xclick';
+                    $pp_img = 'https://www.paypalobjects.com/webstatic/en_US/i/btn/png/btn_buynow_107x26.png'; 
+                    $pp_alt = 'Buy Now';
+                }
+                elseif($pp_select_button =='donate'){
+                    $cmd    = '_donations';
+                    $pp_img = 'https://www.paypalobjects.com/webstatic/en_US/i/btn/png/btn_donate_92x26.png';
+                    $pp_alt = 'Donate';
+                }
+                    
 		$module_class = ET_Builder_Element::add_module_order_class( $module_class, $function_name );
 
 		$module_class .= " et_pb_module et_pb_bg_layout_{$background_layout}";
-
+                //<button type="submit" class="et_pb_button%3$s%5$s" %3$s%4$s>%1$s</button>       
 		$output = sprintf(
 			'<div class="et_pb_button_module_wrapper et_pb_module%6$s">
-                            <form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
+                            <form target="paypal" action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post">                                
                                <input type="hidden" name="business" value="tejasm-merchant@itpathsolutions.co.in">
-                               <input type="hidden" name="cmd" value="_xclick">
+                               <input type="hidden" name="cmd" value="%1$s">
                                <input type="hidden" name="item_name" value="%7$s">
                                <input type="hidden" name="amount" value="%8$s">
                                <input type="hidden" name="currency_code" value="USD">
-                               <button type="submit" class="et_pb_button%3$s%5$s" %3$s%4$s>%1$s</button>       
+                               <input type="image" name="submit" border="0" src="%9$s" alt="%10$s"/>
+                               <img alt="" border="0" width="1" height="1" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" >                               
                             </form>
 			</div>',
-			'' !== $button_text ? esc_html( $button_text ) : '<input type="image" name="submit" border="0" src="https://www.paypalobjects.com/webstatic/en_US/i/btn/png/btn_buynow_107x26.png" alt="Buy Now"/>', 
+			$cmd, 
                         
 			'' !== $custom_icon && 'on' === $button_custom ? sprintf(
 				' data-icon="%1$s"',
@@ -122,7 +144,9 @@ function ex_divi_child_theme_setup1() {
 			'right' === $button_alignment || 'center' === $button_alignment ? sprintf( ' et_pb_button_alignment_%1$s', esc_attr( $button_alignment ) )  : '', 
                         
                         $pp_item_name, 
-                        $pp_amount 
+                        $pp_amount,
+                        $pp_img,
+                        $pp_alt
 		);
 
 		return $output;
@@ -137,7 +161,7 @@ add_action('et_builder_ready', 'ex_divi_child_theme_setup1');
 
 /*
  * Shortcode
- * [et_pb_paypal_button admin_label=”Paypal_Button” url_new_window=”off” button_text=”Paypal Button” button_alignment=”left” background_layout=”light” custom_button=”on” button_letter_spacing=”0″ button_use_icon=”on” button_icon_placement=”right” button_on_hover=”on” button_letter_spacing_hover=”0″ item_name=”Item” pp_amount=”10.00″ text_orientation=”left” button_icon=”%%92%%” button_text_color=”#0c71c3″ button_bg_color=”#ffffff” button_border_color=”#0c71c3″ button_icon_color=”#0c71c3″] [/et_pb_paypal_button]
+ * 
 
  * 
  */
