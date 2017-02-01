5302ed93dc6eccfe5d87426df8fdd0ed28f79713
Andrew Angell <andrew@angelleye.com>
2017-02-01 01:02:39 -0600

    Adjusts text domain value, ref #1

--- paypal-divi.php
+++ paypal-divi.php
@@ -10,7 +10,7 @@
  * Author URI:        http://www.angelleye.com/
  * License:           GNU General Public License v3.0
  * License URI:       http://www.gnu.org/licenses/gpl-3.0.html
- * Text Domain:       angelleye-paypal-divi
+ * Text Domain:       angelleye_paypal_divi
  * Domain Path:       /languages
  */
 
