a01ab3b811b36e67b3cd19adb2f17b970d199dc6
tejas <tejasmht88@gmail.com>
2017-02-06 14:49:27 +0530

    moved to assets directory. ref #7

--- paypal-divi.php
+++ paypal-divi.php
@@ -95,6 +95,6 @@ add_action( 'admin_enqueue_scripts', 'paypal_divi_styles' );
  * Register style sheet decalration.
  */
 function paypal_divi_styles() {
-	wp_register_style( 'paypal_divi', plugins_url( 'css/paypal_divi.css',__FILE__ ) );
+	wp_register_style( 'paypal_divi', plugins_url( 'assets/css/paypal_divi.css',__FILE__ ) );
 	wp_enqueue_style( 'paypal_divi' );
 }
