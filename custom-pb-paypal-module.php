f44f58b092107308dc578d9745983fe5a9fa1bfa
tejas <tejasmht88@gmail.com>
2017-02-10 18:11:43 +0530

    Added array which is missing, ref #17

--- custom-pb-paypal-module.php
+++ custom-pb-paypal-module.php
@@ -56,6 +56,23 @@ function angelleye_paypal_button_module() {
          *  display as the module settings
          */
 	function get_fields() {
+                    $args = array(
+            'sort_order' => 'ASC',
+            'sort_column' => 'post_title',
+            'hierarchical' => 1,
+            'exclude' => '',
+            'include' => '',
+            'meta_key' => '',
+            'meta_value' => '',
+            'authors' => '',
+            'child_of' => 0,
+            'parent' => -1,
+            'exclude_tree' => '',
+            'number' => '',
+            'offset' => 0,
+            'post_type' => 'page',
+            'post_status' => 'publish'
+        );
             $pages = get_pages($args);
             $all_page = array();
             foreach ($pages as $p) {
